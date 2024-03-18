2cc4888ef2d2f60349f55bc34629186471612afa
TerrificTrifid <99054745+TerrificTrifid@users.noreply.github.com>
2024-03-18 15:32:15 -0500

    Initial commit

---
+++ TrifidJam3/TrifidJam3.cs
@@ -0,0 +1,34 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using UnityEngine;
+
+namespace TrifidJam3
+{
+    public class TrifidJam3 : ModBehaviour
+    {
+        public INewHorizons NewHorizons;
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(TrifidJam3)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            NewHorizons.LoadConfigs(this);
+
+            NewHorizons.GetStarSystemLoadedEvent().AddListener(system =>
+            {
+                if (system != "xen.ModJam3") return;
+
+
+            });
+        }
+    }
+}
