68297d1421131a8c69bde71033171c0779332a98
TerrificTrifid <99054745+TerrificTrifid@users.noreply.github.com>
2024-03-20 17:51:09 -0500

    model and progress

--- TrifidJam3/scripts/GrappleController.cs
+++ TrifidJam3/scripts/GrappleController.cs
@@ -1,77 +1,169 @@
-﻿using NewHorizons;
-using NewHorizons.Utility.Files;
+using NewHorizons;
+using NewHorizons.Handlers;
 using NewHorizons.Utility.OWML;
 using UnityEngine;
 
 namespace TrifidJam3;
 
 [UsedInUnityProject]
-public class GrappleController : MonoBehaviour
+public class GrappleController : OWItem
 {
 	public static GrappleController Instance { get; private set; }
-	
+
+	private ScreenPrompt _activatePrompt;
+	private ScreenPrompt _reelInPrompt;
+	private ScreenPrompt _reelOutPrompt;
+
+	public Light Light;
+
 	public AudioClip AmbienceAudio;
-	public AudioClip ReelInAudio;
-	public AudioClip ReelOutAudio;
-	private OWAudioSource _loopAudioSource;
-	public AudioClip ActivateAudio;
+	private OWAudioSource _ambienceAudioSource;
+	public AudioClip ReelAudio;
+    private OWAudioSource _reelAudioSource;
+    public AudioClip ActivateAudio;
 	public AudioClip ReleaseAudio;
 	private OWAudioSource _oneShotAudioSource;
 
 	private bool _grappleActive;
+	private float _grappleValue;
 	private int _reelDirection;
 
-	private void Awake()
+	public override void Awake()
 	{
 		Instance = this;
+		_type = ItemType.VisionTorch;
+		base.Awake();
 	}
 
 	private void Start()
 	{
+		_activatePrompt = new ScreenPrompt(InputLibrary.toolActionPrimary, TranslationHandler.GetTranslation("Activate", TranslationHandler.TextType.UI) + "   <CMD>");
+		_reelInPrompt = new ScreenPrompt(InputLibrary.thrustUp, TranslationHandler.GetTranslation("Reel In", TranslationHandler.TextType.UI) + "   <CMD>");
+		_reelOutPrompt = new ScreenPrompt(InputLibrary.thrustDown, TranslationHandler.GetTranslation("Reel Out", TranslationHandler.TextType.UI) + "   <CMD>");
+
 		var playerAudioController = Locator.GetPlayerAudioController();
 
-        _loopAudioSource = Instantiate(
+		_ambienceAudioSource = Instantiate(
 			playerAudioController._oneShotSource,
 			playerAudioController._oneShotSource.transform.parent
 		);
-		_oneShotAudioSource = Instantiate(
+        _reelAudioSource = Instantiate(
+            playerAudioController._oneShotSource,
+            playerAudioController._oneShotSource.transform.parent
+        );
+        _oneShotAudioSource = Instantiate(
 			playerAudioController._oneShotSource,
 			playerAudioController._oneShotSource.transform.parent
 		);
 
-		Delay.FireOnNextUpdate(() =>
-		{
-			gameObject.SetActive(false);
-		});
+        _ambienceAudioSource.clip = AmbienceAudio;
+        _ambienceAudioSource.loop = true;
+        _ambienceAudioSource.SetMaxVolume(0.1f);
+        _reelAudioSource.clip = ReelAudio;
+        _reelAudioSource.loop = true;
+        _reelAudioSource.SetMaxVolume(0.5f);
+
+        enabled = false;
 	}
 
-	private void OnDestroy()
+	public override void OnDestroy()
 	{
-		Destroy(_loopAudioSource.gameObject);
-		Destroy(_oneShotAudioSource.gameObject);
+		Destroy(_ambienceAudioSource.gameObject);
+        Destroy(_reelAudioSource.gameObject);
+        Destroy(_oneShotAudioSource.gameObject);
+		base.OnDestroy();
 	}
 
-	public bool IsGrappleActive() => _grappleActive;
+	public override string GetDisplayName()
+	{
+		return TranslationHandler.GetTranslation("Threader", TranslationHandler.TextType.UI);
+	}
+
+	public override void PickUpItem(Transform holdTranform)
+	{
+		base.PickUpItem(holdTranform);
+
+		Locator.GetPromptManager().AddScreenPrompt(_activatePrompt, PromptPosition.UpperRight, true);
+		Locator.GetPromptManager().AddScreenPrompt(_reelInPrompt, PromptPosition.UpperRight, true);
+		Locator.GetPromptManager().AddScreenPrompt(_reelOutPrompt, PromptPosition.UpperRight, true);
+
+		enabled = true;
+	}
+
+	public override void DropItem(Vector3 position, Vector3 normal, Transform parent, Sector sector, IItemDropTarget customDropTarget)
+	{
+		base.DropItem(position, normal, parent, sector, customDropTarget);
+			
+		Locator.GetPromptManager().RemoveScreenPrompt(_activatePrompt, PromptPosition.UpperRight);
+		Locator.GetPromptManager().RemoveScreenPrompt(_reelInPrompt, PromptPosition.UpperRight);
+		Locator.GetPromptManager().RemoveScreenPrompt(_reelOutPrompt, PromptPosition.UpperRight);
+
+		enabled = false;
+	}
+
+	public override void SocketItem(Transform socketTransform, Sector sector)
+	{
+		base.SocketItem(socketTransform, sector);
+			
+		Locator.GetPromptManager().RemoveScreenPrompt(_activatePrompt, PromptPosition.UpperRight);
+		Locator.GetPromptManager().RemoveScreenPrompt(_reelInPrompt, PromptPosition.UpperRight);
+		Locator.GetPromptManager().RemoveScreenPrompt(_reelOutPrompt, PromptPosition.UpperRight);
+
+		enabled = false;
+	}
+
+	public override bool CheckIsDroppable()
+	{
+		return !_grappleActive;
+	}
+
+	private void Update()
+	{
+		if (OWInput.IsPressed(InputLibrary.toolActionPrimary, InputMode.Character))
+		{
+			if (!_grappleActive)
+			{
+				NHLogger.Log("activate");
+				ActivateGrapple();
+			}
+		}
+		else if (_grappleActive)
+		{
+			NHLogger.Log("release");
+			ReleaseGrapple();
+		}
+
+		if (OWInput.IsPressed(InputLibrary.thrustUp, InputMode.Character))
+		{
+			NHLogger.Log("reel in");
+			_reelDirection = -1;
+		}
+		else if (OWInput.IsPressed(InputLibrary.thrustDown, InputMode.Character))
+		{
+			NHLogger.Log("reel out");
+			_reelDirection = 1;
+		}
+		else _reelDirection = 0;
+	}
 
 	private void FixedUpdate()
 	{
-		
+		if (_grappleActive)
+		{
+			
+		}
 	}
 
 	public void ActivateGrapple()
 	{
 		_grappleActive = true;
 		_reelDirection = 0;
-    }
+	}
 
 	public void ReleaseGrapple()
 	{
 		_grappleActive = false;
 		_reelDirection = 0;
-    }
-
-	public void Reel(int direction)
-	{
-		_reelDirection = direction;
 	}
+
 }
---
+++ TrifidJam3/scripts/GrapplePatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace TrifidJam3;
+
+[HarmonyPatch]
+public class GrapplePatches : MonoBehaviour
+{
+    [HarmonyPostfix, HarmonyPatch(typeof(ToolModeUI), nameof(ToolModeUI.Update))]
+    private static void ToolModeUI_Update(ToolModeUI __instance)
+    {
+        if (OWInput.IsInputMode(InputMode.Character) && __instance._toolSwapper.GetToolMode() == ToolMode.Item)
+        {
+            if (__instance._toolSwapper.GetItemCarryTool().GetHeldItem() is GrappleController)
+            {
+                __instance._projectPrompt.SetVisibility(false);
+            }
+        }
+    }
+}
