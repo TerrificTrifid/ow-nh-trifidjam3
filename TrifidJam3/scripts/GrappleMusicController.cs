862b91cb947bf1909466438ddf1e5cdd85555699
TerrificTrifid <99054745+TerrificTrifid@users.noreply.github.com>
2024-03-19 22:17:02 -0500

    grapple start

---
+++ TrifidJam3/scripts/GrappleMusicController.cs
@@ -0,0 +1,21 @@
+﻿using NewHorizons;
+using NewHorizons.Components.Props;
+using UnityEngine;
+
+namespace TrifidJam3
+{
+    public class GrappleMusicController : MonoBehaviour
+    {
+        private static NHItem nhItem;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void Start()
+        {
+            nhItem = this.GetComponent<NHItem>();
+        }
+    }
+}
